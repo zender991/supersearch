8cdc2f736a8d77bf9a534193b75b68645e0c43b8
zender991 <zender991@gmail.com>
2017-05-02 18:08:16 +0300

    dep3

---
+++ config.py
@@ -0,0 +1,14 @@
+DEBUG = True
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///testbd.sqlite'
+#SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:pass@localhost:5432/supersearch'
+#SQLALCHEMY_DATABASE_URI = 'postgresql:///supersearch'
+SECRET_KEY = 'zxczxasdsad'
+
+DEVELOPER_KEY = "AIzaSyCjdP4tZZh3eUc_wgxXX6hjdct-mo0Gp34"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+CONSUMER_KEY='RQfEhM8ZeWjHBwYsR5T8Rkivp'
+CONSUMER_SECRET='ib4YMVrccLZCv5vxc3mHumXw102SAcFj7N4N2CWexnbdFv87CJ'
+ACCESS_TOKEN='836684282001297408-bk82K2vxYklhsDPiCME22DnfZAcRluL'
+ACCESS_TOKEN_SECRET='gJYRJ68kVGcM9RxR6e9BWou077a7Jk5te3pQ9HxwbBMih'
